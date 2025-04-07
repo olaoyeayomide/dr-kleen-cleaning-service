00f70a7a434730849ee0ed9d213678d7d51cc0f2
olaoyeayomide <ayomideolaoye84@gmail.com>
2025-04-07 04:18:14 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,54 +6,44 @@ module.exports = {
     "C:/Users/ayomi/Desktop/Project/DrKleen Cleaning Services/dist/drkleen.html",
     "C:/Users/ayomi/Desktop/Project/DrKleen Cleaning Services/dist/drkleen.js",
     "C:/Users/ayomi/Desktop/Project/DrKleen Cleaning Services/dist/test.html",
+    "C:/Users/ayomi/Desktop/Project/DrKleen Cleaning Services/src/input.css",
     "node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-      fontSize: {
-        sm: ["2rem"],
-        base: ["1.6rem", "2rem"],
+        bree: ['"Bree Serif"', "serif"],
+        public: ['"Public Sans"', "sans-serif"],
       },
       colors: {
         primary: "#f5ca4f",
         secondary: "#007bff",
+        // Yellow gradient colors
         gradientStart: "#ffdd52",
         gradientMid: "#f5ca4f",
         gradientEnd: "#c29f3e",
+        // White gradient colors
+        whiteGradientStart: "#ffffff", // pure white
+        whiteGradientMid: "#f3f4f6", // soft light gray
+        whiteGradientEnd: "#ffffff",
       },
 
-      // ANIMATION
-      // keyframes: {
-      //   slideLeft: {
-      //     "0%": { transform: "translateX(0)" },
-      //     "100%": { transform: "translateX(-50%)" },
-      //   },
-      //   slideRight: {
-      //     "0%": { transform: "translateX(0)" },
-      //     "100%": { transform: "translateX(50%)" },
-      //   },
-      // },
-      // animation: {
-      //   slideLeft: "slideLeft 8s linear infinite alternate",
-      //   slideRight: "slideRight 8s linear infinite alternate",
-      // },
+      backgroundImage: {
+        "bubble-bg":
+          "linear-gradient(to left, transparent, #fff 100%), url('https://i.ibb.co/hRBS6xzR/freepik-adjust-80776.png')",
+      },
+
+      // BUBBLE ANIMATION
+      animation: {
+        float: "float 8s infinite ease-in-out",
+      },
+      keyframes: {
+        float: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-20px)" },
+        },
+      },
     },
   },
-  plugins: [
-    plugin(function ({ addUtilities }) {
-      addUtilities({
-        ".magicpattern": {
-          width: "100%",
-          height: "100%",
-          "background-size": "cover",
-          "background-position": "center center",
-          "background-repeat": "repeat",
-          "background-image": "url('data:image/svg+xml;utf8,%3Csvg...')",
-        },
-      });
-    }),
-  ],
+  plugins: [],
 };
